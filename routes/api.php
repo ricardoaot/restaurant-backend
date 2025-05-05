3296d599f1e40ace5344a0a30132c2cbc27a938a
ricardoaot <ricardoaot@gmail.com>
2025-05-05 11:00:44 +0200

    Updated routes based on domain and purpose

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoyaltyController;
 
-Route::get('/loyalty-points', [LoyaltyController::class, 'getPoints']);
-Route::post('/loyalty-points', [LoyaltyController::class, 'storePoints']);
-Route::get('/loyalty-transactions/{email}', [LoyaltyController::class, 'getTransactions']);
-Route::post('/loyalty-redeem', [LoyaltyController::class, 'redeem']);
+Route::get('/loyalty/points', [LoyaltyController::class, 'getPoints']);
+Route::post('/loyalty/points', [LoyaltyController::class, 'storePoints']);
+Route::get('/loyalty/points/transactions/{email}', [LoyaltyController::class, 'getTransactions']);
+Route::post('/loyalty/points/redeem', [LoyaltyController::class, 'redeem']);
